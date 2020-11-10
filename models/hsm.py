f9da643c4560450a4516a15e651cca740c256140
Piotr Swierczynski <pwswierczynski@gmail.com>
2020-11-09 21:28:17 -0500

    Running on a CPU - modifying the model

--- models/hsm.py
+++ models/hsm.py
@@ -43,8 +43,15 @@ class HSMNet(nn.Module):
         diff feature volume
         '''
         width = refimg_fea.shape[-1]
-        cost = Variable(torch.cuda.FloatTensor(refimg_fea.size()[0], refimg_fea.size()[1], maxdisp,  refimg_fea.size()[2],  refimg_fea.size()[3]).fill_(0.))
-        for i in range(min(maxdisp, width)):
+
+        if torch.cuda.is_available():
+            cost = Variable(torch.cuda.FloatTensor(refimg_fea.size()[0], refimg_fea.size()[1], maxdisp,  refimg_fea.size()[2],  refimg_fea.size()[3]).fill_(0.))
+        else:
+            cost = Variable(
+                torch.FloatTensor(refimg_fea.size()[0], refimg_fea.size()[1], maxdisp,
+                                       refimg_fea.size()[2], refimg_fea.size()[3]).fill_(
+                    0.))
+        for i in range(maxdisp):
             feata = refimg_fea[:,:,:,i:width]
             featb = targetimg_fea[:,:,:,:width-i]
             # concat

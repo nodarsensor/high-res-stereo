bc9530b3db2807341a6819038c70d85fb14c5d92
Piotr Swierczynski <pwswierczynski@gmail.com>
2020-11-09 22:27:12 -0500

    Colorizing dispairty

--- run.py
+++ run.py
@@ -16,6 +16,9 @@ from models.submodule import *
 from utils.eval import mkdir_p, save_pfm
 from utils.preprocess import get_transform
 
+from pynodar.filters import sky
+from pynodar.stereo import colorize_disparity_map_u8
+
 # Configuration
 ROOT_DIR = Path()
 WEIGHTS_PATH = ROOT_DIR / "weights" / "final-768px.tar"
@@ -24,13 +27,14 @@ LEFT_RECTIFIED = PATH_TO_IMAGES / "left_rect"
 RIGHT_RECTIFIED = PATH_TO_IMAGES / "right_rect"
 OUTPUT = ROOT_DIR / "output"
 DISPARITY_DIR = OUTPUT / "disparity"
-ENTROPY_DIR = OUTPUT / "entropy"
+COLORED_DISPARITY_DIR = OUTPUT / "colored_disparity"
 
 SCALE = 0.1  # 1.0 does not fit into Nvidia GPU memory
 MAX_DISP = 768
 
 # Create output directories
 os.makedirs(DISPARITY_DIR, exist_ok=True)
+os.makedirs(COLORED_DISPARITY_DIR, exist_ok=True)
 
 # cudnn.benchmark = True
 cudnn.benchmark = False
@@ -60,9 +64,9 @@ model.eval()
 
 all_files = sorted(os.listdir(LEFT_RECTIFIED))
 
-for idx, filename in enumerate(all_files):
+for idx, filename in enumerate(all_files[:5]):
 
-    if not filename.endswith('.png'):
+    if not filename.endswith(".png"):
         continue
 
     # import pdb; pdb.set_trace()
@@ -85,18 +89,10 @@ for idx, filename in enumerate(all_files):
         model.module.maxdisp = 128
 
     if is_cuda_available:
-        model.module.disp_reg8 = disparityregression(
-            model.module.maxdisp, 16
-        ).cuda()
-        model.module.disp_reg16 = disparityregression(
-            model.module.maxdisp, 16
-        ).cuda()
-        model.module.disp_reg32 = disparityregression(
-            model.module.maxdisp, 32
-        ).cuda()
-        model.module.disp_reg64 = disparityregression(
-            model.module.maxdisp, 64
-        ).cuda()
+        model.module.disp_reg8 = disparityregression(model.module.maxdisp, 16).cuda()
+        model.module.disp_reg16 = disparityregression(model.module.maxdisp, 16).cuda()
+        model.module.disp_reg32 = disparityregression(model.module.maxdisp, 32).cuda()
+        model.module.disp_reg64 = disparityregression(model.module.maxdisp, 64).cuda()
     else:
         model.module.disp_reg8 = disparityregression(model.module.maxdisp, 16)
         model.module.disp_reg16 = disparityregression(model.module.maxdisp, 16)
@@ -107,24 +103,20 @@ for idx, filename in enumerate(all_files):
 
     # resize
     img1_resized = cv2.resize(
-        img1r,
-        None,
-        fx=SCALE,
-        fy=SCALE,
-        interpolation=cv2.INTER_CUBIC,
+        img1r, None, fx=SCALE, fy=SCALE, interpolation=cv2.INTER_CUBIC,
     )
     img2_resized = cv2.resize(
-        img2r,
-        None,
-        fx=SCALE,
-        fy=SCALE,
-        interpolation=cv2.INTER_CUBIC,
+        img2r, None, fx=SCALE, fy=SCALE, interpolation=cv2.INTER_CUBIC,
     )
     img1_processed = processed(img1_resized).numpy()
     img2_processed = processed(img2_resized).numpy()
 
-    img1_reshaped = np.reshape(img1_processed, [1, 3, img1_processed.shape[1], img1_processed.shape[2]])
-    img2_reshaped = np.reshape(img2_processed, [1, 3, img2_processed.shape[1], img2_processed.shape[2]])
+    img1_reshaped = np.reshape(
+        img1_processed, [1, 3, img1_processed.shape[1], img1_processed.shape[2]]
+    )
+    img2_reshaped = np.reshape(
+        img2_processed, [1, 3, img2_processed.shape[1], img2_processed.shape[2]]
+    )
 
     # Padding
     max_h = int(img1_reshaped.shape[2] // 64 * 64)
@@ -180,21 +172,32 @@ for idx, filename in enumerate(all_files):
     # save predictions
     # resize to highres
     pred_disp = cv2.resize(
-        pred_disp / SCALE,
-        (img_size[1], img_size[0]),
-        interpolation=cv2.INTER_NEAREST,
+        pred_disp / SCALE, (img_size[1], img_size[0]), interpolation=cv2.INTER_NEAREST,
     )
 
     # clip while keep inf
     invalid = np.logical_or(pred_disp == np.inf, pred_disp != pred_disp)
     pred_disp[invalid] = np.inf
+
+    # Filter out black regions
+    invalid_pixel_mask = pred_disp < 0
+    rect_mask = np.all(img1r == [0, 0, 0], axis=-1)
+    rect_mask_bool = rect_mask.astype(bool)
+    sky_mask = sky(img1r, horizon_row=984, B_thresh=150)
+
+    bad_pixel_mask = invalid_pixel_mask + rect_mask + sky_mask
+    pred_disp[bad_pixel_mask] = 0
+
     disparity_u8 = (255 * pred_disp / MAX_DISP).astype(np.uint8)
+    colored_disp = colorize_disparity_map_u8(
+        disp_u8=disparity_u8, max_disparity=255, bad_pixel_mask=bad_pixel_mask
+    )
 
     path_to_disp = DISPARITY_DIR / filename
+    path_to_colored_disp = COLORED_DISPARITY_DIR / filename
 
     cv2.imwrite(str(path_to_disp), disparity_u8)
+    cv2.imwrite(str(path_to_colored_disp), colored_disp)
 
     if is_cuda_available:
         torch.cuda.empty_cache()
-
-    break
